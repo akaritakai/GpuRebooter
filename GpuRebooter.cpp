5e8821eb1ae245af90f1d6a27a6779c07a4548db
Justin Kaufman <akaritakai@gmail.com>
2015-06-22 13:58:04 -0500

    Replace searching for device manually
    Per shockkolate's advice, use &GUID_DEVCLASS_DISPLAY rather than fuck about with strings.

--- GpuRebooter.cpp
+++ GpuRebooter.cpp
@@ -5,49 +5,26 @@ int _tmain(int, _TCHAR*[])
 	FreeConsole();
 	RegisterHotKey(nullptr, 1, MOD_CONTROL | MOD_ALT, VK_CANCEL);
 	MSG msg = {0};
-	while (GetMessage(&msg, nullptr, 0, 0) != 0)
+	while (GetMessage(&msg, nullptr, 0, 0) != NULL)
 	{
 		if (msg.message == WM_HOTKEY)
 		{
-			auto hDevInfo = SetupDiGetClassDevs(nullptr, REGSTR_KEY_PCIENUM, nullptr, DIGCF_PRESENT | DIGCF_ALLCLASSES);
+			auto hDevInfo = SetupDiGetClassDevs(&GUID_DEVCLASS_DISPLAY, REGSTR_KEY_PCIENUM, nullptr, DIGCF_PRESENT | DIGCF_ALLCLASSES);
 			SP_DEVINFO_DATA deviceInfoData;
 			deviceInfoData.cbSize = sizeof(SP_DEVINFO_DATA);
 			for (DWORD i = 0; SetupDiEnumDeviceInfo(hDevInfo, i, &deviceInfoData); i++)
 			{
-				DWORD dataT;
-				LPTSTR buffer = nullptr;
-				DWORD bufferSize = 0;
-
-				while (!SetupDiGetDeviceRegistryProperty(hDevInfo, &deviceInfoData, SPDRP_DEVICEDESC, &dataT, reinterpret_cast<PBYTE>(buffer), bufferSize, &bufferSize))
-				{
-					if (GetLastError() == ERROR_INSUFFICIENT_BUFFER)
-					{
-						if (buffer) LocalFree(buffer);
-						buffer = static_cast<LPTSTR>(LocalAlloc(LPTR, bufferSize * 2));
-					}
-					else break;
-				}
-
-				if (buffer == nullptr) break;
-
-				if (_tcsncmp(buffer, _T("NVIDIA GeForce GTX 970"), 22) == 0)
-				{
-					SP_PROPCHANGE_PARAMS params;
-					params.ClassInstallHeader.cbSize = sizeof(SP_CLASSINSTALL_HEADER);
-					params.ClassInstallHeader.InstallFunction = DIF_PROPERTYCHANGE;
-					params.Scope = DICS_FLAG_GLOBAL ;
-					params.StateChange = DICS_DISABLE;
-					SetupDiSetClassInstallParams(hDevInfo, &deviceInfoData, &params.ClassInstallHeader, sizeof(params));
-					SetupDiCallClassInstaller(DIF_PROPERTYCHANGE, hDevInfo, &deviceInfoData);
-					params.StateChange = DICS_ENABLE;
-					SetupDiSetClassInstallParams(hDevInfo, &deviceInfoData, &params.ClassInstallHeader, sizeof(params));
-					SetupDiCallClassInstaller(DIF_PROPERTYCHANGE, hDevInfo, &deviceInfoData);
-				}
-
-				if (buffer) LocalFree(buffer);
+				SP_PROPCHANGE_PARAMS params;
+				params.ClassInstallHeader.cbSize = sizeof(SP_CLASSINSTALL_HEADER);
+				params.ClassInstallHeader.InstallFunction = DIF_PROPERTYCHANGE;
+				params.Scope = DICS_FLAG_GLOBAL ;
+				params.StateChange = DICS_DISABLE;
+				SetupDiSetClassInstallParams(hDevInfo, &deviceInfoData, &params.ClassInstallHeader, sizeof(params));
+				SetupDiCallClassInstaller(DIF_PROPERTYCHANGE, hDevInfo, &deviceInfoData);
+				params.StateChange = DICS_ENABLE;
+				SetupDiSetClassInstallParams(hDevInfo, &deviceInfoData, &params.ClassInstallHeader, sizeof(params));
+				SetupDiCallClassInstaller(DIF_PROPERTYCHANGE, hDevInfo, &deviceInfoData);
 			}
-
-			SetupDiDestroyDeviceInfoList(hDevInfo);
 		}
 	}
 
