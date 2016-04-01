ec7a43e1c5952628f1f1983a1abfb5f65019292f
Justin Kaufman <akaritakai@gmail.com>
2016-03-31 23:29:48 -0700

    Minor fixes and formatting changes

--- GpuRebooter.cpp
+++ GpuRebooter.cpp
@@ -2,31 +2,31 @@
 
 int _tmain(int, _TCHAR*[])
 {
-	FreeConsole();
-	RegisterHotKey(nullptr, 1, MOD_CONTROL | MOD_ALT, VK_CANCEL);
-	MSG msg = {0};
-	while (GetMessage(&msg, nullptr, 0, 0) != NULL)
-	{
-		if (msg.message == WM_HOTKEY)
-		{
-			auto hDevInfo = SetupDiGetClassDevs(&GUID_DEVCLASS_DISPLAY, REGSTR_KEY_PCIENUM, nullptr, DIGCF_PRESENT | DIGCF_ALLCLASSES);
-			SP_DEVINFO_DATA deviceInfoData;
-			deviceInfoData.cbSize = sizeof(SP_DEVINFO_DATA);
-			for (DWORD i = 0; SetupDiEnumDeviceInfo(hDevInfo, i, &deviceInfoData); i++)
-			{
-				SP_PROPCHANGE_PARAMS params;
-				params.ClassInstallHeader.cbSize = sizeof(SP_CLASSINSTALL_HEADER);
-				params.ClassInstallHeader.InstallFunction = DIF_PROPERTYCHANGE;
-				params.Scope = DICS_FLAG_GLOBAL ;
-				params.StateChange = DICS_DISABLE;
-				SetupDiSetClassInstallParams(hDevInfo, &deviceInfoData, &params.ClassInstallHeader, sizeof(params));
-				SetupDiCallClassInstaller(DIF_PROPERTYCHANGE, hDevInfo, &deviceInfoData);
-				params.StateChange = DICS_ENABLE;
-				SetupDiSetClassInstallParams(hDevInfo, &deviceInfoData, &params.ClassInstallHeader, sizeof(params));
-				SetupDiCallClassInstaller(DIF_PROPERTYCHANGE, hDevInfo, &deviceInfoData);
-			}
-		}
-	}
+    FreeConsole();
+    RegisterHotKey(nullptr, NULL, MOD_CONTROL | MOD_ALT, VK_CANCEL);
+    MSG msg = { nullptr };
+    while (GetMessage(&msg, nullptr, 0, 0))
+    {
+        if (msg.message == WM_HOTKEY)
+        {
+            auto hDevInfo = SetupDiGetClassDevs(&GUID_DEVCLASS_DISPLAY, REGSTR_KEY_PCIENUM, nullptr, DIGCF_PRESENT | DIGCF_ALLCLASSES);
+            SP_DEVINFO_DATA deviceInfoData;
+            deviceInfoData.cbSize = sizeof(SP_DEVINFO_DATA);
+            for (DWORD i = 0; SetupDiEnumDeviceInfo(hDevInfo, i, &deviceInfoData); i++)
+            {
+                SP_PROPCHANGE_PARAMS params;
+                params.ClassInstallHeader.cbSize = sizeof(SP_CLASSINSTALL_HEADER);
+                params.ClassInstallHeader.InstallFunction = DIF_PROPERTYCHANGE;
+                params.Scope = DICS_FLAG_GLOBAL;
+                params.StateChange = DICS_DISABLE;
+                SetupDiSetClassInstallParams(hDevInfo, &deviceInfoData, &params.ClassInstallHeader, sizeof(params));
+                SetupDiCallClassInstaller(DIF_PROPERTYCHANGE, hDevInfo, &deviceInfoData);
+                params.StateChange = DICS_ENABLE;
+                SetupDiSetClassInstallParams(hDevInfo, &deviceInfoData, &params.ClassInstallHeader, sizeof(params));
+                SetupDiCallClassInstaller(DIF_PROPERTYCHANGE, hDevInfo, &deviceInfoData);
+            }
+        }
+    }
 
-	return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 }
